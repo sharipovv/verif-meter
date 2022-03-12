a4ef34e4dd0196b2154846051afa289d1410bdb8
Aleksandr <sharipov.sgu@gmail.com>
2022-03-12 04:00:57 +0400

    first commit

---
+++ precache-manifest.a029f4d3b3b63508ec5e1ae1bf8e314e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33796478a9669ead3c174c4e520b6d2d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70aa35dc503fb80810f6",
+    "url": "./static/css/2.3f762d4c.chunk.css"
+  },
+  {
+    "revision": "891fe316555b7e28fd35",
+    "url": "./static/css/main.9b590384.chunk.css"
+  },
+  {
+    "revision": "70aa35dc503fb80810f6",
+    "url": "./static/js/2.4cf17f2f.chunk.js"
+  },
+  {
+    "revision": "c9e0c53042442321331287fb1614b34d",
+    "url": "./static/js/2.4cf17f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891fe316555b7e28fd35",
+    "url": "./static/js/main.3083b752.chunk.js"
+  },
+  {
+    "revision": "b9db6bad3fa31e37a30a",
+    "url": "./static/js/runtime-main.5189b5fe.js"
+  }
+]);
