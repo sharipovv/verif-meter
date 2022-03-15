9224d6a439439b72286495217e867f34556ae2d9
Aleksandr <sharipov.sgu@gmail.com>
2022-03-16 02:40:08 +0400

    11

---
+++ precache-manifest.5272d38a886d96062f24bb95eb5ef44a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75676b100fc997d98c1c251c2be3459",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34640deccdea15c4618d",
+    "url": "./static/css/2.53e713cb.chunk.css"
+  },
+  {
+    "revision": "65aaaa736ffa1d193e6f",
+    "url": "./static/css/main.8b281ff5.chunk.css"
+  },
+  {
+    "revision": "34640deccdea15c4618d",
+    "url": "./static/js/2.0ec8ed94.chunk.js"
+  },
+  {
+    "revision": "c9e0c53042442321331287fb1614b34d",
+    "url": "./static/js/2.0ec8ed94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65aaaa736ffa1d193e6f",
+    "url": "./static/js/main.4ef01b81.chunk.js"
+  },
+  {
+    "revision": "b9db6bad3fa31e37a30a",
+    "url": "./static/js/runtime-main.5189b5fe.js"
+  }
+]);
